b31058e458e2e91f25f88132e7d03d9195371cb3
Dennis Vriend <dnvriend@gmail.com>
2015-05-30 20:20:57 +0200

    being declarative with for expressions

--- src/test/scala/com/github/dnvriend/UpdateTest.scala
+++ src/test/scala/com/github/dnvriend/UpdateTest.scala
@@ -19,4 +19,23 @@ class UpdateTest extends TestSpec {
       User(Some(4), "Steve", "Wozniak")
     )
   }
+
+  it should "update a user using for expressions" in {
+
+    val userQuery = for(u <- users if u.first === "Steve" && u.last === "Jobs") yield u
+    
+    val nrUpdated = for {
+      user <- db.run(userQuery.result.head)
+      nrUpdated <- db.run(userQuery.update(user.copy(first = "APPLE", last = "ROCKS")))
+    } yield nrUpdated
+    
+    nrUpdated.futureValue shouldBe 1
+
+    db.run(users.sortBy(_.id).result).futureValue shouldBe List(
+      User(Some(1), "Bill", "Gates"),
+      User(Some(2), "Steve", "Balmer"),
+      User(Some(3), "APPLE", "ROCKS"),
+      User(Some(4), "Steve", "Wozniak")
+    )
+  }
 }
